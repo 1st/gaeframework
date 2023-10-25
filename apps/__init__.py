6d3951ed5c21ac8803ab2020e4d938a8175ae8bf
Anton Danilchenko <anton.danilchenko@gmail.com>
2023-10-25 19:35:24 +0300

    Added apps directory from Google Code
    Moved code from Google Code

---
+++ apps/__init__.py
@@ -0,0 +1,7 @@
+'''
+This is collection of applications that used in different projects.
+
+You can do manage.py install project_name.app_name to create symlink
+in your project directory to this app. One app can be used in the
+different projects.
+'''
